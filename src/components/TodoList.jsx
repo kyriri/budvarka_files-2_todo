29ad53c56a058533b0d8663bc3a04851b7e3b663
kyriri <31296669+kyriri@users.noreply.github.com>
2022-07-13 18:24:19 -0300

    Recap Todo list functioning
    Co-authored-by: Bernardo Sunderhus <bernardo.sunderhus@gmail.com>

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const todos = [
-    'Uma tarefa',
-    'Outra tarefa'
+const initialTasks = [
+  'comer chocolate',
+  'ir velejar',
 ]
 
 export function TodoList() {
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        console.log(event)
-    }
-    return <div>
-        <form onSubmit={handleSubmit}>
-            <input type="text"/>
-        </form>
-        <ul>
-            {todos.map((todo, index) => (
-                <li key={index}>{todo}</li>
-            ))}
-        </ul>
-    </div>
+  const [tasks, setTasks] = useState(initialTasks)
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (inputValue === '') return
+    setTasks([...tasks, inputValue])
+    setInputValue('')
+  }
+  const handleInputChange = (ev) => setInputValue(ev.currentTarget.value.trim())
+  const [inputValue, setInputValue] = useState('')
+
+
+  return <div>
+    <form onSubmit={handleSubmit}>
+      <input value={inputValue} onChange={handleInputChange} type="text" />
+    </form>
+    <ul>
+      {tasks.map((task, index) => <li key={index}>{task}</li>)}
+    </ul>
+  </div>
 }
