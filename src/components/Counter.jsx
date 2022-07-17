98c60b381c84fba4b35bd3dd284d3c64ad7844c7
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2022-07-17 10:07:37 +0000

    feat: adds jest test env

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export function Counter({initialCount = 0}) {
+export function Counter({initialCount = 0, ...props}) {
     const [count, setCount] = React.useState(initialCount)
     const handleIncrement = () => setCount(count + 1)
     const handleDrecement = () => setCount(count - 1)
-    return <div>
+    return <div {...props}>
         <button onClick={handleIncrement}>+</button>
         <span>{count}</span>
         <button onClick={handleDrecement}>-</button>
