a887e6b51f51134545e06f0a43cea6909911ec27
kyriri <31296669+kyriri@users.noreply.github.com>
2022-07-20 01:03:41 -0300

    feat: Add filter buttons for tasks

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,20 +1,18 @@
 import React, { useState } from 'react'
 
 const initialTasks = [
-  {
-    name: 'comer chocolate',
-    isDone: true,
-  },
-  {
-    name: 'velejar',
-    isDone: false,
-  },
+  { name: 'comer chocolate', isDone: true},
+  { name: 'velejar', isDone: false},
+  { name: 'tomar banho', isDone: false},
+  { name: 'varrer a casa', isDone: true},
+  { name: 'preparar almoço', isDone: false},
 ]
 
 export function TodoList() {
   const DoneTaskStyle = {textDecoration: 'line-through'}
   const [tasks, setTasks] = useState(initialTasks)
   const [inputValue, setInputValue] = useState('')
+  const [tasksToShow, setTasksToShow] = useState(initialTasks)
   
   const handleInputChange = (ev) => setInputValue(ev.currentTarget.value.trim())
   const handleSubmit = (event) => {
@@ -34,12 +32,22 @@ export function TodoList() {
     setTasks(nextTasks)
   };
 
+  const handleTaskFilters = (ev) => {
+    if (ev.target.id === 'done_tasks') setTasksToShow(tasks.filter(item => item.isDone))
+    if (ev.target.id === 'pending_tasks') setTasksToShow(tasks.filter(item => !item.isDone))
+    if (ev.target.id === 'all_tasks') setTasksToShow(tasks)
+  }
+
   return <div>
     <form onSubmit={handleSubmit}>
       <input value={inputValue} onChange={handleInputChange} type="text" />
     </form>
+    <br />
+    <button onClick={handleTaskFilters} id='all_tasks'>All tasks</button>
+    <button onClick={handleTaskFilters} id='done_tasks'>Done</button>
+    <button onClick={handleTaskFilters} id='pending_tasks'>Pending</button>
     <ul>
-      {tasks.map((task, index) => <li onClick={() => handleClickOnTask(task)} style={task.isDone ? DoneTaskStyle : {}} key={index}>{task.name}</li>)}
+      {tasksToShow.map((task, index) => <li onClick={() => handleClickOnTask(task)} style={task.isDone ? DoneTaskStyle : {}} key={index}>{task.name}</li>)}
     </ul>
   </div>
 }
